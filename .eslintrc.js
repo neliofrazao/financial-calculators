f0823645d8b1dae2baf18c5ad525934fb162915d
Nelio Frazao <neliofrazac@gmail.com>
2020-01-29 14:30:09 -0300

    chore: add rules for typeScript in Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+  parser:  '@typescript-eslint/parser',
   extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
-    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 9,
--- src/app/interfaces/createElement.interface.ts
+++ src/app/interfaces/createElement.interface.ts
@@ -1,4 +1,4 @@
 export interface createElementInterface {
   tag: string;
-  className: string;
+  className?: string;
 }
