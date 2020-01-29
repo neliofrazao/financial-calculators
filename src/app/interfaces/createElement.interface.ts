9f9ff0002e5d990179cb7dd36ddc332600981fb0
Nelio Frazao <neliofrazac@gmail.com>
2020-01-29 18:59:51 -0300

    refactor: Improve class name

--- src/app/interfaces/createElement.interface.ts
+++ src/app/interfaces/createElement.interface.ts
@@ -1,4 +1,4 @@
-export interface createElementInterface {
+export interface CreateElementInterface {
   tag: string;
   className?: string;
 }
--- src/app/utils/createElement.ts
+++ src/app/utils/createElement.ts
@@ -1,11 +1,11 @@
-import { createElementInterface } from '../interfaces/createElement.interface'
+import { CreateElementInterface } from '../interfaces/createElement.interface';
 
-export class createElement {
-  public static setHtmlElement({ tag, className }: createElementInterface) {
+export class CreateElement {
+  public static setHtmlElement({ tag, className }: CreateElementInterface): HTMLElement {
     const createHtmlElement = document.createElement(tag);
     const hasClassName = typeof className !== 'undefined';
     hasClassName && createHtmlElement.classList.add(className);
-  
+
     return createHtmlElement;
   }
 }
