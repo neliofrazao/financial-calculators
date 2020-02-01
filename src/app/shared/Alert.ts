6d7115de487fa392133fbe8310221a9aa4fc81be
Nelio Frazao <neliofrazac@gmail.com>
2020-02-01 02:11:24 -0300

    feat: add handleAlertTimeout and handleCloseAlert function

--- src/app/shared/Alert.ts
+++ src/app/shared/Alert.ts
@@ -38,7 +38,25 @@ export class Alert {
         },
       ],
     });
-
+    this.handleAlertTimeout();
+    this.handleCloseAlert();
     return document.body.appendChild(alertRoot);
+  }
+
+  private handleAlertTimeout(): void {
+    setTimeout(() => {
+      const getAlert = document.getElementById('alert');
+      getAlert.remove();
+    }, 3000);
+  }
+
+  private handleCloseAlert(): void {
+    setTimeout(() => {
+      const getAlert = document.getElementById('alert');
+      const getCloseButton = document.getElementById('close');
+      getCloseButton.addEventListener('click', () => {
+        getAlert.remove();
+      });
+    }, 100);
   }
 }
