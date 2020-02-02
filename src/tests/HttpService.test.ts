1d3a67288225d85ea92bf2b0e7dceb5be79f2788
Nelio Frazao <neliofrazac@gmail.com>
2020-02-02 15:15:26 -0300

    test: should show offline message

--- src/tests/HttpService.test.ts
+++ src/tests/HttpService.test.ts
@@ -3,7 +3,6 @@ import { FetchMock } from 'jest-fetch-mock';
 
 const fetchMock = fetch as FetchMock;
 
-const navigatorOnline = jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(true);
 const payload = {
   amount: 15000,
   installments: 3,
@@ -13,10 +12,22 @@ const payload = {
 describe('testing http service', () => {
   beforeEach(() => {
     fetchMock.resetMocks();
+    jest.clearAllMocks();
+  });
+
+  test('should show offline message', async () => {
+    jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(false);
+    const url = 'hash-front-test.herokuapp.com/';
+    const alertMessage = 'Para utilizar o nosso sistema é necessário estar conectado a uma Rede';
+    await HttpService.post(url, payload);
+    const getAlert = document.getElementById('alert');
+
+    expect(fetchMock).toHaveBeenCalledTimes(0);
+    expect(getAlert.innerHTML).toContain(alertMessage);
   });
 
   test('should post method have been called once', async () => {
-    navigatorOnline;
+    jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(true);
     const url = 'hash-front-test.herokuapp.com/';
     await HttpService.post(url, payload);
 
