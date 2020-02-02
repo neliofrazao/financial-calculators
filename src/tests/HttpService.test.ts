ec3963e7a340168b7e511fda9faf66b69ee46513
Nelio Frazao <neliofrazac@gmail.com>
2020-02-02 16:03:40 -0300

    test: should print error message

--- src/tests/HttpService.test.ts
+++ src/tests/HttpService.test.ts
@@ -1,4 +1,4 @@
-import { HttpService } from '../app/utils';
+import { HttpService, errorMessages } from '../app/utils';
 import { FetchMock } from 'jest-fetch-mock';
 
 const fetchMock = fetch as FetchMock;
@@ -15,28 +15,39 @@ describe('testing http service', () => {
     jest.clearAllMocks();
   });
 
-  test('should show offline message', async () => {
-    jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(false);
-    const url = 'hash-front-test.herokuapp.com/';
-    const alertMessage = 'Para utilizar o nosso sistema é necessário estar conectado a uma Rede';
-    await HttpService.post(url, payload);
-    const getAlert = document.getElementById('alert');
-
-    expect(fetchMock).toHaveBeenCalledTimes(0);
-    expect(getAlert.innerHTML).toContain(alertMessage);
-  });
-
   test('should post method have been called once', async () => {
     jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(true);
     const url = 'hash-front-test.herokuapp.com/';
     await HttpService.post(url, payload);
 
     expect(fetchMock).toHaveBeenCalledTimes(1);
-    expect(fetch).toHaveBeenCalledWith('https://hash-front-test.herokuapp.com/', {
+    expect(fetchMock).toHaveBeenCalledWith('https://hash-front-test.herokuapp.com/', {
       'body': '{"amount":15000,"installments":3,"mdr":6}',
       'headers': { 'Content-type': 'application/json', 'credentials': 'include' },
       'method': 'POST',
       'mode': 'cors',
     });
   });
+
+  test('should print error message', async () => {
+    jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(true);
+    fetchMock.mockRejectedValueOnce(Promise.resolve({}));
+
+    const url = 'hash-front-test.herokuapp.com/';
+    await HttpService.post(url, payload);
+    const getAlert = document.getElementById('alert');
+
+    expect(getAlert.innerHTML).toContain(errorMessages.ERROR_DEFAULT);
+  });
+
+  test('should print offline message', async () => {
+    jest.spyOn(window.navigator, 'onLine', 'get').mockReturnValue(false);
+    const url = 'hash-front-test.herokuapp.com/';
+    await HttpService.post(url, payload);
+    const getAlert = document.getElementById('alert');
+
+    expect(fetchMock).toHaveBeenCalledTimes(0);
+    expect(getAlert.innerHTML).toContain(errorMessages.NETWORK_ERROR);
+  });
+
 });
