db51aa3dea9efd7534382dea47b6f96653964686
Nelio Frazao <neliofrazac@gmail.com>
2020-02-02 22:44:37 -0300

    test: shoud output div ReceiptsList with default values for another dates

--- src/tests/ReceiptsList.test.ts
+++ src/tests/ReceiptsList.test.ts
@@ -1,28 +1,52 @@
 import { ReceiptsList } from '../app/components/ReceiptsList/ReceiptsList';
 
 describe('test ReceiptsList', () => {
-  const initialValues = [
-    {
-      date: 'Amanhã',
-      value: 12436,
-    },
-    {
-      date: 'Em 15 dias',
-      value: 12831,
-    },
-  ];
-
   test('shoud output div ReceiptsList with data and values', () => {
+    const initialValues = [
+      {
+        date: 'Amanhã',
+        value: 12436,
+      },
+      {
+        date: 'Em 15 dias',
+        value: 12831,
+      },
+      {
+        date: 'Em 30 dias',
+        value: 11094,
+      },
+      {
+        date: 'Em 90 dias',
+        value: 12900,
+      },
+    ];
+
     const newList = new ReceiptsList();
     const list = newList.getReceiptsList(initialValues);
     document.body.appendChild(list);
 
     const getHtmlElement = document.getElementById('receipts');
     const getItemListElement = getHtmlElement.getElementsByTagName('li');
-    const getSpanElement = getHtmlElement.getElementsByTagName('span');
 
     expect(getItemListElement).toBeDefined();
-    expect(getItemListElement.length).toBe(2);
-    expect(getSpanElement[0].innerHTML).toContain('12,436.00');
+    expect(getItemListElement.length).toBe(4);
+  });
+
+  test('shoud output div ReceiptsList with default values for another dates', () => {
+    const initialValues = [
+      {
+        date: 'Amanhã',
+        value: 12436,
+      },
+    ];
+    const newList = new ReceiptsList();
+    const list = newList.getReceiptsList(initialValues);
+    document.body.appendChild(list);
+
+    const getHtmlElement = document.getElementById('receipts');
+    const getItemListElement = getHtmlElement.getElementsByTagName('li');
+
+    expect(getItemListElement).toBeDefined();
+    expect(getItemListElement.length).toBe(4);
   });
 });
